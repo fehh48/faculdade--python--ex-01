96449c334df716850b253cc2d96e355248f06b42
fehh48 <99373683+fehh48@users.noreply.github.com>
2022-08-08 21:27:14 -0300

    first commit

---
+++ facu.py
@@ -0,0 +1,15 @@
+print("linguagem python", end='...')
+print('texto')
+
+print('texto1', 'texto2', 'texto3', sep='-')
+
+print('texto1', 'texto2', 'texto3', sep='\n')
+
+print("Nome\tRa")
+print("Felipe\t2022015")
+print("Jamis\t2545156")
+print("Jose\t2054534")
+
+pi = 3.141592
+print("p valor de PI é: {:.3f}".format(pi))
+print(f"p valor de PI é: {pi}")
